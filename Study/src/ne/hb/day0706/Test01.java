96993dea987ad82c4d38747ad7761cb399439e17
gksqlc6401 <gksqlc6401@naver.com>
2021-07-06 17:57:38 +0900

    07/06

---
+++ Study/src/ne/hb/day0706/Test01.java
@@ -0,0 +1,16 @@
+package ne.hb.day0706;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+    System.out.println("수업끝!");
+    System.out.println("집가자!");
+    System.out.println("피곤행");
+    System.out.println("담배피고싶당");
+  }
+}
+
+
+
+
+
