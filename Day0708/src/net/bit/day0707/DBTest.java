76ca57dd3ff6654d800ab99f7353495d2a4d0ae3
gksqlc6401 <gksqlc6401@naver.com>
2021-07-07 17:52:20 +0900

    07/07

---
+++ Day0708/src/net/bit/day0707/DBTest.java
@@ -0,0 +1,10 @@
+package net.bit.day0707;
+
+import java.sql.Connection;
+
+public class DBTest {
+
+  public static void main(String[] args) {
+    Connection CN=null; 
+  }//main end
+}//class end
